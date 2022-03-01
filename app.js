af5c2021ce83f54f8a7eeee9f563ffef5ed4a1ca
RishavJain21 <86527173+RishavJain21@users.noreply.github.com>
2022-03-01 19:40:20 +0530

    Connecting app.js to index.html

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set("view engine", "html");
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.redirect("/index")
+})
+
+app.get('/index',function(req,res) {
+  res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(port, () => {
